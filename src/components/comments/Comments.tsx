1e1a504c8b2c50a2f263ac08bf183247fe18a5f6
Sujeong Ji <suji.sujeongji@gmail.com>
2023-09-30 23:30:52 +0900

    feat: add single post page section

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./comments.module.css";
 import Link from "next/link";
+import Image from "next/image";
 import SectionTitle from "@/components/sectionTitle/SectionTitle";
 
 const Comments = () => {
@@ -11,13 +12,36 @@ const Comments = () => {
       <SectionTitle title="Comments" />
       {status === "authenticated" ? (
         <div className={styles.write}>
-          write
-          <textarea placeholder="write a comment" className={styles.input} />
+          <textarea placeholder="Write a comment" className={styles.input} />
           <button className={styles.button}>send</button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment </Link>
       )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/logo.png"
+              alt="user"
+              width={42}
+              height={42}
+              priority
+              className={styles.userImage}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.userName}>Deoqua</span>
+              <span className={styles.date}>2023.11.11</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque earum
+            voluptatem praesentium expedita deleniti eaque, ut vitae, iusto
+            velit assumenda aut culpa sint eligendi delectus. Consectetur
+            voluptates sed nulla sequi.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
