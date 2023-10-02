c67fb639cf977331a4909d32de48daf955d97327
Sujeong Ji <suji.sujeongji@gmail.com>
2023-10-03 08:15:57 +0900

    feat: set prisma client and schema

---
+++ prisma/schemaTypes.ts
@@ -0,0 +1,76 @@
+export interface Account {
+  _id: string;
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refresh_token?: string | null;
+  access_token?: string | null;
+  expires_at?: number | null;
+  token_type?: string | null;
+  scope?: string | null;
+  id_token?: string | null;
+  session_state?: string | null;
+  user: User;
+}
+
+export interface Session {
+  _id: string;
+  sessionToken: string;
+  userId: string;
+  expires: Date;
+  user: User;
+}
+
+export interface User {
+  _id: string;
+  name?: string | null;
+  email: string;
+  emailVerified?: Date | null;
+  image?: string | null;
+  accounts: Account[];
+  sessions: Session[];
+  posts: Post[];
+  comments: Comment[];
+}
+
+export interface VerificationToken {
+  // identifier: string;
+  _id: string;
+  token: string;
+  expires: Date;
+}
+
+export interface Category {
+  _id: string;
+  slug: string;
+  title: string;
+  img: string | null;
+  posts: Post[];
+}
+
+export interface Post {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  slug: string;
+  title: string;
+  desc: string;
+  img?: string;
+  views: number;
+  catSlug: string;
+  cat: Category;
+  userEmail: string;
+  user: User;
+  comments: Comment[];
+}
+
+export interface Comment {
+  _id: string;
+  createdAt: Date;
+  desc: string;
+  userEmail: string;
+  user: User;
+  postSlug: string;
+  post: Post;
+}
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,8 +2,13 @@ import GoogleProvider from "next-auth/providers/google";
 // import GithubProvider from "next-auth/providers/github";
 // import FacebookProvider from "next-auth/providers/facebook";
 import { getServerSession } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+import prisma from "@/utils/connect";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_ID as string,
