4c92b61a69b5f6a11c5b91e61c38bfdf9f99bb4b
Sujeong Ji <suji.sujeongji@gmail.com>
2023-10-09 21:57:25 +0900

    feat: update featured section

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,31 +1,54 @@
 import React from "react";
 import styles from "./featured.module.css";
 import Image from "next/image";
-import { Roboto } from "next/font/google";
+import Link from "next/link";
 
-const Featured = () => {
+const getData = async (slug: string) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      console.error("Fetch error:", res.status, res.statusText);
+      throw new Error("Failed");
+    }
+    return res.json();
+  } catch (error) {
+    console.error("getData error:", error);
+    throw error;
+  }
+};
+
+const Featured = async () => {
+  const post = await getData("remove-cache");
+
   return (
     <div className="styles.container">
       <div className={styles.post}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="/p1.jpeg"
-            alt="featured"
-            fill
-            sizes="100%"
-            className={styles.image}
-            priority
-            style={{ width: "100%", height: "100%" }}
-          />
-        </div>
+        {post.img && (
+          <div className={styles.imgContainer}>
+            <Image
+              src={post.img}
+              alt="featured"
+              fill
+              sizes="100%"
+              className={styles.image}
+              priority
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        )}
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>New features in Next.js 13</h1>
-          <p className={styles.postDesc}>
-            Next.js 13 introduces a powerful new Image component, allowing you
-            to easily display images without layout shift and optimize files
-            on-demand for increased performance.
-          </p>
-          <button className={styles.button}>Read More</button>
+          <h1 className={styles.postTitle}>{post.title}</h1>
+          <p
+            className={styles.postDesc}
+            dangerouslySetInnerHTML={{ __html: post.desc }}
+          />
+          {/* <button className={styles.button}>Read More</button> */}
+          <Link href={`/posts/${post.slug}`} className={styles.link}>
+            Read More ➚
+          </Link>
         </div>
       </div>
     </div>
