c386689177d8b1c694e73a175039a1ee1fb9dd69
Sujeong Ji <suji.sujeongji@gmail.com>
2023-10-14 22:45:10 +0900

    feat: add spinner

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./loading.module.css";
+import spinner from "../../../public/spinner.svg";
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className={styles.container}>
+      <Image
+        src={spinner}
+        alt="Loading"
+        className={styles.spinner}
+        style={{ margin: "auto" }}
+      />
+    </div>
+  );
+};
+
+export default Loading;
